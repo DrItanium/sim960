b39f71e12e1b39aa1f0759871eef225131b60ac2
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-11-27 13:15:31 -0800

    Basic invocation body

--- i960_Prototype_Simulator.ino
+++ i960_Prototype_Simulator.ino
@@ -1,14 +1,16 @@
-#include <SPI.h>
 #include <array>
 #include <type_traits>
 #include <string>
 #include <map>
+#include <memory>
 #include <SD.h>
+#include <SPI.h>
 #include "TargetPlatform.h"
 #include "PinSetup.h"
 #include "SetupSerial.h"
 #include "Core.h"
-
+#include "MemoryInterface.h"
+std::unique_ptr<i960::Core> theCore = nullptr;
 /// @todo implement the register frames "in hardware"
 void setup() {
     setupSerial(9600);
@@ -16,10 +18,12 @@ void setup() {
     Serial.print("Starting up SPI...");
     SPI.begin();
     Serial.println("Done");
+    theCore = std::make_unique<i960::Core>(i960::getStubMemoryController());
 }
 
 void loop() {
+    theCore->cycle();
   // put your main code here, to run repeatedly:
     //Serial.println("Donuts");
-    delay(1000);
+    delay(1);
 }
