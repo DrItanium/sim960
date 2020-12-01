985ca1a9ae96a16b619adac305b56a61d3e3d341
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-12-01 15:45:43 -0800

    Enable TFT Shield for Metro M4

--- MetroM4Express.cpp
+++ MetroM4Express.cpp
@@ -6,6 +6,19 @@
 #include <SPI.h>
 #include <Arduino.h>
 #include <SD.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <Adafruit_seesaw.h>
+#include <Adafruit_TFTShield18.h>
+
+// this design has the adafruit 1.8" TFT shield with microsd card 5-way nav + 3 buttons shield attached to the top
+Adafruit_TFTShield18 ss;
+constexpr auto SD_CS = 4;
+constexpr auto TFT_CS = 10;
+constexpr auto TFT_DC = 8;
+constexpr auto TFT_RESET = -1;
+
+Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_RESET);
 
 Integer
 MetroM4ExpressBoard::loadValue(Address address, TreatAsInteger)  {
@@ -55,7 +68,38 @@ void
 MetroM4ExpressBoard::storeValue(Address address, ShortInteger value, TreatAsShortInteger)  {
 
 }
+void
+startupTFTShield() {
+   pinMode(TFT_CS, OUTPUT);
+   digitalWrite(TFT_CS, HIGH);
+   pinMode(SD_CS, OUTPUT);
+   digitalWrite(SD_CS, HIGH);
 
+   if (!ss.begin()) {
+       Serial.println("seesaw could not be initialized!");
+       while(true);
+   }
+   Serial.println("seesaw started");
+   Serial.print("Version: ");
+   Serial.println(ss.getVersion(), HEX);
+
+   // disable the backlight to start
+   ss.setBacklight(TFTSHIELD_BACKLIGHT_OFF);
+   ss.tftReset();
+
+   tft.initR(INITR_BLACKTAB);
+   Serial.println("TFT OK!");
+   tft.fillScreen(ST77XX_GREEN);
+
+   Serial.print("Initializing SD Card...");
+   if (!SD.begin(SD_CS)) {
+       Serial.println("failed!");
+   } else {
+       Serial.println("OK!");
+   }
+
+
+}
 void
 MetroM4ExpressBoard::begin() {
     // Unlike the grand central m4, the metro m4 does not have a built in sd card slot, I will need to use a shield for this purpose instead.
@@ -66,9 +110,7 @@ MetroM4ExpressBoard::begin() {
         while (!Serial);
         Serial.println("i960 Simulator Starting up");
         pinMode(LED_BUILTIN, OUTPUT);
-        Serial.print("Starting up SPI...");
-        SPI.begin();
-        Serial.println("Done");
+        startupTFTShield();
     }
 }
 #endif // defined ADAFRUIT_METRO_M4_EXPRESS || defined ARDUINO_METRO_M4
