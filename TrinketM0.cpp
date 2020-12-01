6d1c798ceca7e1d92364b382e980fd90a0273366
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-12-01 14:56:39 -0800

    Start targeting the trinket m0 as my base target for now.
    I was using the grand central m4 but the trinket m0 fits all of my code just fine with plenty left over.

---
+++ TrinketM0.cpp
@@ -0,0 +1,91 @@
+//
+// Created by jwscoggins on 11/27/20.
+//
+#if defined(ARDUINO_TRINKET_M0) || defined(ADAFRUIT_TRINKET_M0)
+#include "TrinketM0.h"
+#include <SPI.h>
+#include <Arduino.h>
+#include <SD.h>
+
+Integer
+TrinketM0Board::loadValue(Address address, TreatAsInteger)  {
+    return 0;
+}
+Ordinal
+TrinketM0Board::loadValue(Address address, TreatAsOrdinal)  {
+    return 0;
+}
+void
+TrinketM0Board::storeValue(Address address, Ordinal value, TreatAsOrdinal) {
+
+}
+void
+TrinketM0Board::storeValue(Address address, Integer value, TreatAsInteger)  {
+
+}
+ByteInteger
+TrinketM0Board::loadValue(Address address, TreatAsByteInteger)  {
+    return 0;
+}
+ByteOrdinal
+TrinketM0Board::loadValue(Address address, TreatAsByteOrdinal)  {
+    return 0;
+}
+void
+TrinketM0Board::storeValue(Address address, ByteOrdinal value, TreatAsByteOrdinal)  {
+
+}
+void
+TrinketM0Board::storeValue(Address address, ByteInteger value, TreatAsByteInteger)  {
+
+}
+ShortInteger
+TrinketM0Board::loadValue(Address address, TreatAsShortInteger)  {
+    return 0;
+}
+ShortOrdinal
+TrinketM0Board::loadValue(Address address, TreatAsShortOrdinal)  {
+    return 0;
+}
+void
+TrinketM0Board::storeValue(Address address, ShortOrdinal value, TreatAsShortOrdinal)  {
+
+}
+void
+TrinketM0Board::storeValue(Address address, ShortInteger value, TreatAsShortInteger)  {
+
+}
+
+void
+TrinketM0Board::begin() {
+    /*
+     * For now, the Grand Central M4 uses an SD Card for its memory with a small portion of the on board sram used for scratchpad / always
+     * available memory. On board devices are mapped into the implicit onboard device area of [0xFF00'0000,0xFFFF'FFFF]. The IO device bus
+     * exists here as extra "modules" such as:
+     * - Clock Generator
+     * - RTC
+     * - ESP32 Wifi Coprocessor
+     * - Display
+     * - Sharp Memory Display
+     * - GPIOs from the Grand Central
+     * - SPI IO Expanders
+     * - Speaker
+     * - Control Registers
+     * - 2 MB Onboard SPI Flash directly mapped into IO Device Space
+     * - OPL3 Device
+     * - Neopixels
+     * - etc
+     */
+    if (!_initialized) {
+        _initialized = true;
+        Serial.begin(9600);
+        while (!Serial);
+        Serial.println("i960 Simulator Starting up");
+        pinMode(LED_BUILTIN, OUTPUT);
+        Serial.print("Starting up SPI...");
+        SPI.begin();
+        Serial.println("Done");
+    }
+}
+#endif // end defined(ARDUINO_GRAND_CENTRAL_M4) || defined(ADAFRUIT_GRAND_CENTRAL_M4)
+
--- TrinketM0.h
+++ TrinketM0.h
@@ -4,7 +4,28 @@
 
 #ifndef I960_PROTOTYPE_SIMULATOR_TRINKET_M0_H
 #define I960_PROTOTYPE_SIMULATOR_TRINKET_M0_H
-
 #include "MemoryInterface.h"
-using TargetBoard = StubMemoryInterface;
+#if defined(ARDUINO_TRINKET_M0) || defined(ADAFRUIT_TRINKET_M0)
+class TrinketM0Board : public MemoryInterface {
+public:
+    using MemoryInterface::MemoryInterface;
+    ~TrinketM0Board() override = default;
+    Integer loadValue(Address address, TreatAsInteger) override;
+    Ordinal loadValue(Address address, TreatAsOrdinal) override;
+    void storeValue(Address address, Ordinal value, TreatAsOrdinal) override;
+    void storeValue(Address address, Integer value, TreatAsInteger) override;
+    ByteInteger loadValue(Address address, TreatAsByteInteger) override;
+    ByteOrdinal loadValue(Address address, TreatAsByteOrdinal) override;
+    void storeValue(Address address, ByteOrdinal value, TreatAsByteOrdinal) override;
+    void storeValue(Address address, ByteInteger value, TreatAsByteInteger) override;
+    ShortInteger loadValue(Address address, TreatAsShortInteger) override;
+    ShortOrdinal loadValue(Address address, TreatAsShortOrdinal) override;
+    void storeValue(Address address, ShortOrdinal value, TreatAsShortOrdinal) override;
+    void storeValue(Address address, ShortInteger value, TreatAsShortInteger) override;
+    void begin() override;
+private:
+    bool _initialized = false;
+};
+using TargetBoard = TrinketM0Board;
+#endif // end defined(ARDUINO_TRINKET_M0) || defined(ADAFRUIT_TRINKET_M0)
 #endif //I960_PROTOTYPE_SIMULATOR_TRINKET_M0_H
