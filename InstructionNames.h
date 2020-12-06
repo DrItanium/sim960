08419085fa13e6e3d6605ca1fb7d333508fd92dc
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-12-05 17:54:31 -0800

    Inline InstructionNames methods

--- InstructionNames.h
+++ InstructionNames.h
@@ -9,7 +9,30 @@
 #include <optional>
 #include "CoreTypes.h"
 namespace i960 {
-    std::optional<std::string> getNameFromOpcode(ShortOrdinal opcode) noexcept;
-    std::optional<ShortOrdinal> getOpcodeFromName(const std::string& name) noexcept;
+    inline std::optional<std::string> getNameFromOpcode(ShortOrdinal opcode) noexcept {
+        static const std::map<ShortOrdinal, std::string> table {
+#define X(opcode, name) { static_cast<ShortOrdinal>(opcode), #name },
+#include "OpcodesRaw.h"
+#undef X
+        };
+        if (auto result = table.find(opcode); result != table.cend()) {
+            return std::make_optional(result->second);
+        } else {
+            return std::nullopt;
+        }
+    }
+
+    inline std::optional<ShortOrdinal> getOpcodeFromName(const std::string& name) noexcept {
+        static const std::map<std::string, ShortOrdinal> table {
+#define X(opcode, name) { #name, static_cast<ShortOrdinal>(opcode) },
+#include "OpcodesRaw.h"
+#undef X
+        };
+        if (auto result = table.find(name); result != table.cend()) {
+            return std::make_optional(result->second);
+        } else {
+            return std::nullopt;
+        }
+    }
 }
 #endif //SIM960_INSTRUCTIONNAMES_H
