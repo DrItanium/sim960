f79aa3ab207f48012be5442215f085db784e8937
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-12-05 17:48:25 -0800

    Opcodes.def is now OpcodesRaw.h
    The Arduino IDE doesn't see Opcodes.def during building

--- InstructionNames.cpp
+++ InstructionNames.cpp
@@ -9,7 +9,7 @@ namespace i960 {
     getNameFromOpcode(ShortOrdinal opcode) noexcept {
         static const std::map<ShortOrdinal, std::string> table {
 #define X(opcode, name) { static_cast<ShortOrdinal>(opcode), #name },
-#include "Opcodes.def"
+#include "OpcodesRaw.h"
 #undef X
         };
         if (auto result = table.find(opcode); result != table.cend()) {
@@ -23,7 +23,7 @@ namespace i960 {
     getOpcodeFromName(const std::string& name) noexcept {
         static const std::map<std::string, ShortOrdinal> table {
 #define X(opcode, name) { #name, static_cast<ShortOrdinal>(opcode) },
-#include "Opcodes.def"
+#include "OpcodesRaw.h"
 #undef X
         };
         if (auto result = table.find(name); result != table.cend()) {
