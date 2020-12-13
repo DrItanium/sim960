cdbad55d1976e9a8bf66c6100fc484518daa3628
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-12-12 18:22:05 -0800

    Operators

--- GenericNumber.h
+++ GenericNumber.h
@@ -6,13 +6,46 @@
 #define SIM960_GENERICNUMBER_H
 #include <type_traits>
 
-template<typename T, size_t width>
+template<typename T, std::size_t width>
 class GenericNumber {
 public:
     GenericNumber(T val = static_cast<T>(0)) : _val(val) { }
-    [[nodiscard]] constexpr auto get() const noexcept { return _val; }
-    void set(T value) noexcept { _val = value; }
+    [[nodiscard]] [[maybe_unused]] constexpr auto get() const noexcept { return _val; }
+    [[maybe_unused]] void set(T value) noexcept { _val = value; }
 private:
     T _val : width;
 };
+template<typename T, std::size_t width>
+bool
+operator==(const GenericNumber<T, width>& a, const GenericNumber<T, width>& b) noexcept {
+    return  a.get() == b.get();
+}
+
+template<typename T, std::size_t width>
+bool
+operator!=(const GenericNumber<T, width>& a, const GenericNumber<T, width>& b) noexcept {
+    return  a.get() != b.get();
+}
+
+template<typename T, std::size_t width>
+GenericNumber<T, width>
+operator+(const GenericNumber<T, width>& a, const GenericNumber<T, width>& b) noexcept {
+    return GenericNumber<T, width>(a.get() + b.get());
+}
+template<typename T, std::size_t width>
+GenericNumber<T, width>
+operator-(const GenericNumber<T, width>& a, const GenericNumber<T, width>& b) noexcept {
+    return GenericNumber<T, width>(a.get() - b.get());
+}
+template<typename T, std::size_t width>
+GenericNumber<T, width>
+operator*(const GenericNumber<T, width>& a, const GenericNumber<T, width>& b) noexcept {
+    return GenericNumber<T, width>(a.get() * b.get());
+}
+
+template<typename T, std::size_t width>
+GenericNumber<T, width>
+operator/(const GenericNumber<T, width>& a, const GenericNumber<T, width>& b) noexcept {
+    return GenericNumber<T, width>(a.get() / b.get());
+}
 #endif //SIM960_GENERICNUMBER_H
