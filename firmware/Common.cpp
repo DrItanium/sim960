8c6ab60a1d9b95348bdc9c012c9815ed3e570d75
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-12-22 15:42:23 -0800

    Memory access routines

--- firmware/Common.cpp
+++ firmware/Common.cpp
@@ -6,6 +6,17 @@
 
 namespace firmware
 {
+    volatile uint32_t&
+    ioSpaceRegister(Address offset) {
+        uint32_t fixed = 0x00FFFFFF & offset;
+        return memory<uint32_t>(fixed | IOSpaceStart);
+    }
+    volatile uint32_t&
+    internalRegister(uint16_t offset) {
+        uint32_t fixed = 0x00FF0000 | offset;
+        return ioSpaceRegister(fixed);
+    }
+
 }
 
 extern "C" {
@@ -13,3 +24,7 @@ extern "C" {
 
     }
 }
+
+
+
+
--- firmware/Common.h
+++ firmware/Common.h
@@ -6,12 +6,17 @@
 #define SIM960_COMMON_H
 #include <stdint.h>
 #include <string>
+typedef uint32_t Address;
 namespace firmware {
-
+    const Address IOSpaceStart = 0xFF000000;
+    const Address InternalRegistersStart = 0xFFFF0000;
+    const Address InternalRegistersEnd = 0xFFFFFFFF;
     template<typename T>
-    volatile T& memory(const uint32_t address) {
+    volatile T& memory(const Address address) {
         return *reinterpret_cast<T*>(address);
     }
+    volatile uint32_t& ioSpaceRegister(Address offset);
+    volatile uint32_t& internalRegister(uint16_t offset);
 } // end namespace firmware
 extern "C" {
     void bootup(void);
